c789087a1aca5dbdf92b14714f399428bb102faa
zocalop <jcb2861@gmail.com>
2026-06-07 21:20:33 -0300

    Updated ProductList.jsx in main

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -5,6 +5,7 @@ import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
 
     const plantsArray = [
         {
